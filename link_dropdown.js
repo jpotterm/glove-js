85a945ffaba4b0562b040a1626b2ba9a9448b5e1
Jonathan Potter <jpotterm@gmail.com>
2015-11-24 11:03:48 -0500

    Update documentation for LinkDropdown

--- link_dropdown.js
+++ link_dropdown.js
@@ -1,6 +1,8 @@
 // -----------------------------------------------------------------------------
 // A simple dropdown for a menu of links. This does not work as a select
-// replacement because the options must be simple anchor tags.
+// replacement because the options must be simple anchor tags. Element must have
+// a child that is a <button> with the class "link-dropdown-button" because
+// this uses focus events.
 //
 // Options
 //     element: The DOM element that will be the dropdown.
